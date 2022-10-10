2246ded1bc9e122a766f4036704556405668ddcc
Debajyoti Dhar <debajyotidhar111@gmail.com>
2022-10-11 02:34:26 +0530

    Added border around each div (#13)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 import React from "react"
-
+import style from "../styles/card.module.css"
 const Card = (props) => {
 
     return (
-        <div>
+        <div className={style.card}>
             {props.name}
             <img src = {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id + 1}.png`} alt = ""></img>
         </div>
