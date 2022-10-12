5b3ff589d48d2d3ea2dcad5937b0dabde94fb6e2
Kartikay Tiwari <96720944+ishtails@users.noreply.github.com>
2022-10-13 03:38:03 +0530

    added vibrant card hover effect! (#24)

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,8 @@ const Card = (props) => {
   return (
     <div
       className="m-5 p-1 w-[200px] h-[150px] shadow-lg rounded-lg flex flex-col items-center cursor-pointer hover:shadow-2xl transition-all
-        bg-pink-50 bg-clip-padding backdrop-filter bg-opacity-30"
+        bg-pink-50 bg-clip-padding backdrop-filter bg-opacity-30 hover:scale-110
+        hover:bg-gradient-to-r from-purple-400 to-pink-300 hover:text-gray-100"
     >
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id + 1}.png`} alt=""></img>
       {props.name}
