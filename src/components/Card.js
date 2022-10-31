9e494c9f68aa1f7eb62564fd41a4bdd281ae831d
Raja Yashwanth Avantsa <94122883+R-Yash@users.noreply.github.com>
2022-10-31 19:41:04 +0530

    Added Pokemon types (#36)

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 const Card = (props) => {
   const [abilityName, setAbilityName] = useState('')
+  const [type1, setType1Name] = useState('')
+  const [type2, setType2Name] = useState('')
   const [abilityDesc, setAbilityDesc] = useState('')
   const [toggleInfo, setToggleInfo] = useState(false)
   const getAbility = async (idx) => {
@@ -18,6 +20,8 @@ const Card = (props) => {
         return
       }
     })
+    setType1Name(res.data.types[0].type.name.toUpperCase())
+    setType2Name(res.data.types[1].type.name.toUpperCase())
   }
   return (
     <div>
@@ -33,6 +37,11 @@ const Card = (props) => {
       </div>
       {toggleInfo && (
         <div className="ml-3 border-4 border-grey-500  absolute flex flex-col items-center w-[220px]  bg-white rounded-lg  p-2">
+          <div>
+            <p className="text-sm font-bold">
+              {type1 || 'Loading'} {type2}
+            </p>
+          </div>
           <h1 className="text-lg font-bold">{abilityName || 'Loading'}</h1>
           <p className="text-sm">{abilityDesc || 'Loading'}</p>
         </div>
